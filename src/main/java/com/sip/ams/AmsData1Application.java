9ffd84ece0a3386388387ab68e14ac1ead1915fd
khouloudbslm <khouloudbslm92@hotmail.com>
2022-04-20 09:15:38 +0100

    nouveau projet

---
+++ src/main/java/com/sip/ams/AmsData1Application.java
@@ -0,0 +1,13 @@
+package com.sip.ams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmsData1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmsData1Application.class, args);
+	}
+
+}
